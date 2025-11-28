efc27eacb4b312e8e35237dec4ee652e36a8169e
Héctor Alejandro Godinez Reyes <aalej.rys@gmail.com>
2025-11-28 12:28:47 -0600

    Primera versión de la práctica Streamlit

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Hola Mundo en Streamlit")
+
+st.write("Esta es mi primera aplicación web creada en Python usando Streamlit.")
+st.subheader("¿Qué hace esta aplicación?")
+
+st.write(
+    """
+    - Muestra un título en la parte superior.
+    - Muestra un texto de bienvenida.
+    - Corre como una aplicación web en el navegador.
+    """
+)
+
+st.success("¡Felicidades! Ya tienes tu primera app web en funcionamiento.")
